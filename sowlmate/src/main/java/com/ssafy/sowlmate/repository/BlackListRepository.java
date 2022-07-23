1fec0b602873acc38796ea98c5ddcdd0917ed413
Younghwan <younghwankim624@gmail.com>
2022-07-23 13:53:24 +0900

    Feat : add blacklist entity, repository, service

---
+++ sowlmate/src/main/java/com/ssafy/sowlmate/repository/BlackListRepository.java
@@ -0,0 +1,28 @@
+package com.ssafy.sowlmate.repository;
+
+import com.ssafy.sowlmate.entity.BlackList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface BlackListRepository extends JpaRepository<BlackList, Long> {
+    public BlackList findByFromUserNoAndToUserNo(long fromUserNo, long toUserNo);
+
+    public BlackList findByFromUSerIdAndToUserId(String fromUserId, String toUserId);
+
+    @Modifying
+    @Transactional
+    public void deleteByFromUserNoAndToUserNo(long fromUserNo, long toUserNo);
+
+    @Modifying
+    @Transactional
+    public void deleteByFromUserIdAndToUserId(String fromUserId, String toUserId);
+
+    List<BlackList> findAllByFromUserNo(long fromUserNo);
+
+    List<BlackList> findAllByFromUserId(String fromUserId);
+}
---
+++ sowlmate/src/main/java/com/ssafy/sowlmate/service/BlackListService.java
@@ -0,0 +1,35 @@
+package com.ssafy.sowlmate.service;
+
+import com.ssafy.sowlmate.entity.BlackList;
+import com.ssafy.sowlmate.repository.BlackListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BlackListService {
+
+    private final BlackListRepository blackListRepository;
+
+    public List<BlackList> selectAllByFromUserId(String fromUserId) {
+        return blackListRepository.findAllByFromUserId(fromUserId);
+    }
+
+    public BlackList selectByFromUserIdAndToUserId(String fromUserId, String toUserId) {
+        return blackListRepository.findByFromUSerIdAndToUserId(fromUserId, toUserId);
+    }
+
+    @Transactional
+    public BlackList enrollBlackList(BlackList blackList) {
+        return blackListRepository.save(blackList);
+    }
+
+    @Transactional
+    public void deleteByFromUserIdAndToUserId(String fromUserId, String toUserId) {
+        blackListRepository.deleteByFromUserIdAndToUserId(fromUserId, toUserId);
+    }
+}
