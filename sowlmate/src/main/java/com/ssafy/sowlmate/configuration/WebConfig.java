d084a4760ea3b9a7a6e76a7a3e27f9bc69645ebf
Younghwan <younghwankim624@gmail.com>
2022-07-27 01:24:41 +0900

    Feat : backend server demo ver

--- sowlmate/src/main/java/com/ssafy/sowlmate/configuration/WebConfig.java
+++ sowlmate/src/main/java/com/ssafy/sowlmate/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+//                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(6000);
