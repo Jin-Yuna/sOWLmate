22c4591d0d248d2e8f1dfdf2e41afc00b84822e6
younghwani <younghwankim624@gmail.com>
2022-07-28 10:28:18 +0900

    Feat : add categories api and modify enum types

---
+++ sowlmate/src/test/java/com/ssafy/sowlmate/controller/CategoryTest.java
@@ -0,0 +1,26 @@
+package com.ssafy.sowlmate.controller;
+
+import com.ssafy.sowlmate.entity.type.InterestType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryTest {
+
+    @Test
+    public void 관심사Enum테스트() {
+        // given
+        InterestType[] values = InterestType.values();
+
+        // when
+        for (InterestType value : values) {
+            System.out.println(value);
+        }
+
+        // then
+
+    }
+}
