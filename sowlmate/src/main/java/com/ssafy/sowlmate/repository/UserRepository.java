c0b51a6182958b299e5f90ae5afdd6d1b83d1708
soobin <asdfls@naver.com>
2022-08-01 15:50:04 +0900

    Fix : modify for merge onto dev

--- sowlmate/src/main/java/com/ssafy/sowlmate/repository/UserRepository.java
+++ sowlmate/src/main/java/com/ssafy/sowlmate/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,6 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     public User findByNo(Long no);
 
     public User findById(String userId);
+
+//    public Optional<User> findById(String userId);
 
     public User findByNickname(String nickname);
 
