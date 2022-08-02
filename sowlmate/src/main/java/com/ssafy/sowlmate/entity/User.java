8e96c71b90857b1f8bf38cdf63edd4977d30aa3b
Younghwan <younghwankim624@gmail.com>
2022-08-02 10:47:15 +0900

    Fix : comment google login

--- sowlmate/src/main/java/com/ssafy/sowlmate/entity/User.java
+++ sowlmate/src/main/java/com/ssafy/sowlmate/entity/User.java
@@ -2,46 +2,41 @@ package com.ssafy.sowlmate.entity;
 
 import com.ssafy.sowlmate.entity.type.LanguageType;
 import com.ssafy.sowlmate.entity.type.RegionType;
-import com.ssafy.sowlmate.entity.type.Role;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @Entity
 @Getter @Setter
 @RequiredArgsConstructor
 @Table(name = "user")
-public class User implements OAuth2User {
+//public class User implements OAuth2User {
+public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long no;
 
-    @NotNull
-    private String id;
-    @NotNull
-    private String password;
-    @NotNull
-    private String nickname;
-    @NotNull
-    private String name;
+    @NotNull private String id;
+    @NotNull private String password;
+    @NotNull private String nickname;
+    @NotNull private String name;
 
     @NotNull
     @Enumerated(EnumType.STRING)
-    private RegionType region; // 대륙 enum type
+    private RegionType region; // 7대륙
 
     @Enumerated(EnumType.STRING)
     private LanguageType language;
+
+    @Enumerated(EnumType.STRING)
+    private LanguageType preferenceLanguage;
 
     @Column(nullable = true)
     private String profilePictureUrl;
@@ -58,27 +53,22 @@ public class User implements OAuth2User {
         this.enrollDate = LocalDateTime.now();
     }
 
-    @Override
-    public Map<String, Object> getAttributes() {
-        return null;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
+    // google auth
 //    @Override
-//    public String getName() {
-//        return id;
+//    public Map<String, Object> getAttributes() {
+//        return null;
 //    }
-
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    public User updateUser(User updateUser) {
-        this.setProfilePictureUrl(updateUser.profilePictureUrl);
-        return this;
-    }
-
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
+//
+//    public User updateUser(User updateUser) {
+//        this.setProfilePictureUrl(updateUser.profilePictureUrl);
+//        return this;
+//    }
 }
