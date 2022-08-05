dc4dd6dc28db5e254e61558926865a5c04f24243
Yuna Jin <dbsdk100@gmail.com>
2022-08-05 17:52:18 +0900

    Feat : complete create room form

--- front/src/components/Room/RoomCreateForm.vue
+++ front/src/components/Room/RoomCreateForm.vue
@@ -8,29 +8,43 @@
         id="title"
         v-model="newRoom.title"
       />
-      <v-chip-group v-model="newRoom.interest" column>
-        <v-chip v-for="interest in InterestList" :key="interest">
+      <h4>내 관심사 중 하나만 선택 가능합니다!</h4>
+      <v-chip-group v-model="newRoom.interest" color="000000" column>
+        <v-chip v-for="interest in userInterest" :key="interest">
           {{ interest }}
         </v-chip>
       </v-chip-group>
-      <input
-        type="checkbox"
-        id="checkbox"
-        v-model="newRoom.locks"
-        true-value="LOCK"
-        false-value="UNLOCK"
-      />
-      <label for="checkbox">
-        <p v-if="newRoom.locks === 'UNLOCK'">LOCK</p>
-        <p v-if="newRoom.locks === 'LOCK'">UNLOCK</p>
-      </label>
-      <v-btn type="submit">생성</v-btn>
+      <div>
+        <input
+          type="checkbox"
+          id="checkbox"
+          true-value="LOCK"
+          false-value="UNLOCK"
+          v-model="newRoom.locks"
+          @click="lockCheck()"
+        />
+        <label for="checkbox" v-if="newRoom.locks === 'UNLOCK'"> LOCK </label>
+        <label for="checkbox" v-if="newRoom.locks === 'LOCK'"> UNLOCK </label>
+      </div>
+      <div>
+        <label for="password" v-if="newRoom.locks === 'LOCK'"
+          >방 비밀번호 :
+        </label>
+        <input
+          type="text"
+          placeholder="방 비밀번호를 지정해주세요."
+          id="password"
+          v-if="newRoom.locks === 'LOCK'"
+          v-model="newRoom.password"
+        />
+      </div>
+      <v-btn type="submit" :disabled="!newRoom.title">생성</v-btn>
     </form>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions, mapMutations } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'RoomCreateForm',
@@ -38,14 +52,44 @@ export default {
     return {
       newRoom: {
         title: '',
-        ownerId: this.userInfo.id,
+        ownerId: '',
         interest: '',
-        language: this.userInfo.language,
+        language: '',
         locks: 'UNLOCK',
+        password: '',
       },
+      userInterest: [],
     };
   },
-  ...mapGetters(['userInfo']),
+  computed: {
+    ...mapGetters(['InterestList', 'userInfo']),
+  },
+  methods: {
+    ...mapActions(['userInterestSave']),
+    interestCheck() {
+      for (const userinterest of this.userInfo.interests) {
+        if (this.InterestList.includes(userinterest['title'])) {
+          this.userInterest.push(userinterest['title']);
+        }
+      }
+    },
+    userInfoCheck() {
+      this.userInfo.id = this.newRoom.ownerId;
+      this.userInfo.language = this.newRoom.languagec;
+    },
+    lockCheck() {
+      let lock = this.newRoom.locks;
+      if (lock === 'LOCK') {
+        lock = 'UNLOCK';
+      } else {
+        lock = 'LOCK';
+      }
+    },
+  },
+  mounted() {
+    this.interestCheck();
+    this.userInfoCheck();
+  },
 };
 </script>
 
