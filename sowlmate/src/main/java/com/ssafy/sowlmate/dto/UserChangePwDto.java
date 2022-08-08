37b6c7eb3749e71b445e6fa7c772aa39b603b333
younghwani <younghwankim624@gmail.com>
2022-08-08 11:19:53 +0900

    Feat : add change password

---
+++ sowlmate/src/main/java/com/ssafy/sowlmate/dto/UserChangePwDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.sowlmate.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class UserChangePwDto {
+    private String userId;
+    private String currentPW;
+    private String newPW;
+}
