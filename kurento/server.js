071e8071b0444b9a8d5e8544a7a43d4a68f2529c
Younghwan <younghwankim624@gmail.com>
2022-08-09 19:42:45 +0900

    Chore : modify port

--- kurento/server.js
+++ kurento/server.js
@@ -9,7 +9,7 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
   default: {
-      as_uri: "https://i7b308.p.ssafy.io:8433/",
+      as_uri: "https://i7b308.p.ssafy.io:8443/",
       ws_uri: "ws://i7b308.p.ssafy.io:8888/kurento"
   }
 });
