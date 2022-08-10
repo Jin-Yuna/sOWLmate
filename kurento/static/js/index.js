64a718cf9ba12ee5d0252271d804884f2cef67b1
younghwani <younghwankim624@gmail.com>
2022-08-10 14:27:53 +0900

    Chore : uncomment replace

--- kurento/static/js/index.js
+++ kurento/static/js/index.js
@@ -279,7 +279,7 @@ function stop(message) {
 		}
 	}
 	hideSpinner(videoInput, videoOutput);
-	// location.replace("https://i7b308.p.ssafy.io")
+	location.replace("https://i7b308.p.ssafy.io")
 }
 
 function sendMessage(message) {
