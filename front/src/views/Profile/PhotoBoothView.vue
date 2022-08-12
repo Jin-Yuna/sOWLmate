d3b39bbe61753f7cfb6aaa01ce7057024688eeb4
younghwani <younghwankim624@gmail.com>
2022-08-12 14:44:42 +0900

    Feat : add image list

--- front/src/views/Profile/PhotoBoothView.vue
+++ front/src/views/Profile/PhotoBoothView.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Photo Booth</h1>
     <button @click="getPhotos">사진불러오기</button>
+    <!--이미지 소스 리스트 -> 이미지 카드 뷰 -> 클릭 시 확대된 모달창-->
+    <ul>
+      <li v-for="photo in photos" :key="photo.no">
+        <img :src="photo.pictureUrl" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,6 +18,11 @@ import axios from 'axios';
 
 export default {
   name: 'PhotoBoothView',
+  data() {
+    return {
+      photos: [],
+    };
+  },
   components: {},
   methods: {
     getPhotos() {
@@ -24,7 +35,8 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          this.photos = response.data;
+          console.log(this.photos);
         })
         .catch((error) => {
           console.error(error);
