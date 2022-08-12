1480f9a5486da0fda5be625176811dbc3a02f7b6
Younghwan <younghwankim624@gmail.com>
2022-08-12 12:47:00 +0900

    Feat : add getPhotos by user

--- front/src/views/Profile/PhotoBoothView.vue
+++ front/src/views/Profile/PhotoBoothView.vue
@@ -1,9 +1,40 @@
 <template>
   <div>
     <h1>Photo Booth</h1>
+    <button @click="getPhotos">사진불러오기</button>
   </div>
 </template>
 
-<script></script>
+<script>
+import { mapGetters } from 'vuex';
+import sowl from '@/api/sowl.js';
+import axios from 'axios';
+
+export default {
+  name: 'PhotoBoothView',
+  components: {},
+  methods: {
+    getPhotos() {
+      console.log(this.currentUser);
+      console.log(sowl.photoBooth.userPhotos());
+      axios
+        .get(`${sowl.photoBooth.userPhotos()}`, {
+          headers: {
+            userId: this.currentUser,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  computed: {
+    ...mapGetters(['currentUser']),
+  },
+};
+</script>
 
 <style></style>
