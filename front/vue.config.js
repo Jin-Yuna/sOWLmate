89ebeff0a6c815caa18b7b2d15e52e6b831ee2fb
younghwani <younghwankim624@gmail.com>
2022-08-18 15:53:25 +0900

    Chore : turn off https setting

--- front/vue.config.js
+++ front/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: 'all',
-    https: true,
+    https: false,
   },
   pluginOptions: {
     vuetify: {
