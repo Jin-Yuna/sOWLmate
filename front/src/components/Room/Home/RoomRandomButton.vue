71526f9d712be721bc5352f846bd3adce6b5c311
Yuna Jin <dbsdk100@gmail.com>
2022-08-19 01:07:36 +0900

    Fix : fix room random and enter url

--- front/src/components/Room/Home/RoomRandomButton.vue
+++ front/src/components/Room/Home/RoomRandomButton.vue
@@ -9,16 +9,7 @@ import { mapGetters, mapActions, mapMutations } from 'vuex';
 
 export default {
   computed: {
-    ...mapGetters([
-      'roomAll',
-      'roomByInterest',
-      'roomByLanguage',
-      'roomByInterestLanguage',
-      'userInfo',
-      'userList',
-      'InterestList',
-    ]),
-    ...mapMutations(['TO_USER_NICKNAME', 'FROM_USER_NICKNAME']),
+    ...mapGetters(['roomAll', 'userInfo', 'userList']),
   },
   data() {
     return {
@@ -27,69 +18,33 @@ export default {
         conferenceNo: '',
       },
       room: {},
+      toUserNickname: '',
     };
   },
   methods: {
-    ...mapActions(['joinRoom']),
+    ...mapActions(['joinRoom', 'getAllRoomList']),
+    ...mapMutations(['TO_USER_NICKNAME', 'FROM_USER_NICKNAME']),
     randomRoom() {
       this.FROM_USER_NICKNAME(this.userInfo.nickname);
-      this.userDta.userId = this.userInfo.id;
-      var conference;
-      if (this.roomByInterestLanguage.length != 0) {
-        conference = Math.floor(
-          Math.random() * this.roomByInterestLanguage.length,
-        );
-        this.userData.conferenceNo =
-          this.roomByInterestLanguage[conference].conferenceNo;
+      this.userData.userId = this.userInfo.id;
 
-        this.room = this.roomByInterestLanguage[conference];
-        for (const user of this.userList) {
-          if (this.room.ownerId === user['id']) {
-            this.TO_USER_NICKNAME(user['nickname']);
-            break;
-          }
-        }
-        return this.joinRoom(this.userData);
-      } else if (this.roomByInterest.length != 0) {
-        conference = Math.floor(Math.random() * this.roomByInterest.length);
-        this.userData.conferenceNo =
-          this.roomByInterest[conference].conferenceNo;
+      var conference = Math.floor(Math.random() * this.roomAll.length);
+      this.userData.conferenceNo = this.roomAll[conference].conferenceNo;
 
-        this.room = this.roomByInterest[conference];
-        for (const user of this.userList) {
-          if (this.room.ownerId === user['id']) {
-            this.TO_USER_NICKNAME(user['nickname']);
-            break;
-          }
-        }
-        return this.joinRoom(this.userData);
-      } else if (this.roomByLanguage.length != 0) {
-        conference = Math.floor(Math.random() * this.roomByInterest.length);
-        this.userData.conferenceNo =
-          this.roomByLanguage[conference].conferenceNo;
+      this.room = this.roomAll[conference];
 
-        this.room = this.roomByLanguage[conference];
-        for (const user of this.userList) {
-          if (this.room.ownerId === user['id']) {
-            this.TO_USER_NICKNAME(user['nickname']);
-            break;
-          }
+      for (let user of this.userList) {
+        if (this.room.ownerId === user['id']) {
+          this.TO_USER_NICKNAME(user['nickname']);
+          break;
         }
-        return this.joinRoom(this.userData);
-      } else {
-        conference = Math.floor(Math.random() * this.roomAll.length);
-        this.userData.conferenceNo = this.roomAll[conference].conferenceNo;
-
-        this.room = this.roomAll[conference];
-        for (const user of this.userList) {
-          if (this.room.ownerId === user['id']) {
-            this.TO_USER_NICKNAME(user['nickname']);
-            break;
-          }
-        }
-        return this.joinRoom(this.userData);
       }
+
+      return this.joinRoom(this.userData);
     },
+  },
+  mounted() {
+    this.getAllRoomList();
   },
 };
 </script>
